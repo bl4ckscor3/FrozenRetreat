8e4c6709b5cceba2272e72daffae24a69eee9e33
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-04 20:13:51 +0100

    fix winterberry bushes dropping sweet berries when rightclicked

--- src/main/java/frozenretreat/block/WinterberryBushBlock.java
+++ src/main/java/frozenretreat/block/WinterberryBushBlock.java
@@ -2,20 +2,45 @@ package frozenretreat.block;
 
 import frozenretreat.registration.FRItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.SweetBerryBushBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
 public class WinterberryBushBlock extends SweetBerryBushBlock {
 	public WinterberryBushBlock(Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+		int age = state.getValue(AGE);
+		boolean isMature = age == 3;
+
+		if (!isMature && player.getItemInHand(hand).is(Items.BONE_MEAL))
+			return InteractionResult.PASS;
+		else if (age > 1) {
+			int amountOfBerries = 1 + level.random.nextInt(2);
+
+			popResource(level, pos, new ItemStack(FRItems.WINTERBERRIES.get(), amountOfBerries + (isMature ? 1 : 0)));
+			level.playSound(null, pos, SoundEvents.SWEET_BERRY_BUSH_PICK_BERRIES, SoundSource.BLOCKS, 1.0F, 0.8F + level.random.nextFloat() * 0.4F);
+			level.setBlock(pos, state.setValue(AGE, 1), 2);
+			return InteractionResult.sidedSuccess(level.isClientSide);
+		}
+
+		return InteractionResult.PASS;
 	}
 
 	@Override
